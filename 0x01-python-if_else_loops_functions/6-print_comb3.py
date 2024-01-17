d453fb6dbe7213b695c91250f75835604d87c6bb
Collins Boakye <colly0087@gmail.com>
2024-01-16 17:46:12 -0800

    edit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for x in range(10):
     for y in range(x + 1, 10):
         if x != y:
             if x < 8:
-                print(f"{x}{y}", end=', ')
+                print("{}{}".format(x, y), end=', ')
             else:
                 print(f"{x}{y}")
                 
