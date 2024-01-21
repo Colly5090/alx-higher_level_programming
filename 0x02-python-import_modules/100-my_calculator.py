b791fb0bbd36024555cfe9b3b86631fbafbcb43a
Collins Boakye <colly0087@gmail.com>
2024-01-20 16:45:27 -0800

    cal

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   import sys
-   from calculator_1 import add, sub, mul, div
+    import sys
+    from calculator_1 import add, sub, mul, div
 
-   if len(sys.argv) != 4:
-       print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-       sys.exit(1)
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
@@ -21,4 +21,3 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-   
