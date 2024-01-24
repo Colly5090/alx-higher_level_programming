d7e54d96405e2815ead78090b0039d0bfec66a1f
Collins Boakye <colly0087@gmail.com>
2024-01-24 13:20:41 -0800

    best

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    highest_score = None
+    highest_key = None
+
+    for key, value in a_dictionary.items():
+        if highest_score is None or value > highest_score:
+            highest_score = value
+            highest_key = key
+    return highest_key
