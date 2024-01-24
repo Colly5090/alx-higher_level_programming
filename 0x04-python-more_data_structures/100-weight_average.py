41ea8d855d1a97436521c3715a21b83186fb444f
Collins Boakye <colly0087@gmail.com>
2024-01-24 15:22:54 -0800

    ave

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    result = 0
+    total_weight = 0
+    for score, weight in my_list:
+        result += score * weight
+        total_weight += weight
+
+    return result / total_weight if total_weight else 0
