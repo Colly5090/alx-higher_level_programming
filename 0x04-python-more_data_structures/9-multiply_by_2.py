0d8818c7be1db406ea6abc96c65c7b5a99f3c22a
Collins Boakye <colly0087@gmail.com>
2024-01-24 09:36:43 -0800

    mul

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
+    new_dict = a_dictionary.copy()
+    new_list = list(new_dict.keys())
 
-    for key, value in a_dictionary.items:
-        new_dict[key] = value * 2
-        result = '\
-                '.join([f'{key}: {value}' for key, value in new_dict.items()])
-    return result_str
+    for i in new_list:
+        new_dict[i] *= 2
+
+    return new_dict
