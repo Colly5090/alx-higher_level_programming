f79304f3871bfdf24cbb9fad1c90faf4eb23fb19
Collins Boakye <colly0087@gmail.com>
2024-02-10 13:57:23 -0800

    divides elements in a list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,4 +16,4 @@ def list_division(my_list_1, my_list_2, list_length):
             return 0
         finally:
             list_result.append(result)
-        return list_result
+    return list_result
