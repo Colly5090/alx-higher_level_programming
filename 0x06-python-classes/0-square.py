b33423654c4128e4e1b8123c47e7dc98af7371bd
Collins Boakye <colly0087@gmail.com>
2024-02-12 07:50:53 -0800

    an empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+""" A Module to define a square, its shape and properties """
+
 class Square:
-    ''' A class to define a square '''
+    """
+    A class to define a square
+    This class defines the basic geometric functionalities of a sqaure
+
+    Attributes:
+        None currently defined
+    
+    Methods:
+        None currently defined
+    """
     pass
