73f6e7dc8f9c6a57d839c18834b4319f1c1dbe24
Collins Boakye <colly0087@gmail.com>
2024-02-12 11:42:05 -0800

    making size private

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A Module to define a square, its shape and properties """
+
+class Square:
+    """
+    A class to define a square
+    This class defines the basic geometric functionalities of a sqaure
+
+    Attributes:
+        A private size as functionality of the square
+    
+    Methods:
+        None currently defined
+    """
+    def __init__(self, size):
+        self.__size = size
