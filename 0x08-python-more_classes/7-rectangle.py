cb606616bdc900e142324310b304e8cec59b40d7
Collins Boakye <colly0087@gmail.com>
2024-02-17 13:53:24 -0800

    string representation from public class attribute

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return str()
         else:
-            rows = [str(Rectangle.print_symbol) * self.__width
+            rows = [str(self.print_symbol) * self.__width
                     for _ in range(self.__height)]
             return "\n".join(rows)
 
