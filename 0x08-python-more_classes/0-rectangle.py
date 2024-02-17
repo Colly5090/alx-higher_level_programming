7bc85f859c359d275481b7ff096d305e173858f6
Collins Boakye <colly0087@gmail.com>
2024-02-17 05:28:07 -0800

    an empty rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" A Module to define Rectangle, its shape and properties """
+
+
+class Rectangle:
+    """
+    A class the define rectangle, its shape and properties
+
+    Attributes:
+        None currently defined 
+    Methods:
+        None currently defined
+    """
+    pass
