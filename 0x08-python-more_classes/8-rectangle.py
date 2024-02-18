c36bc198fe399383d99696ae7431096e886f3005
Collins Boakye <colly0087@gmail.com>
2024-02-18 03:34:37 -0800

    creating static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -74,7 +74,7 @@ class Rectangle:
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
-    
+
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_1, Rectangle):
@@ -84,4 +84,3 @@ class Rectangle:
         if rect_1.area == rect_2.area:
             return rect_1
         return rect_1 if rect_1.area() > rect_2.area() else rect_2
-    
