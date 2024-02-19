0f4a8de4c9c761ba95feb87c0e95b6d71a6971d9
Collins Boakye <colly0087@gmail.com>
2024-02-19 02:07:20 -0800

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+A Function to return list of attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """ Returns list of attributes and methods """
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,2 @@
+0x0A. Python - Inheritance
+This project focus on inheritance, multiple inheritance and multi-level inheritance and ensuring documentation and testing each script
