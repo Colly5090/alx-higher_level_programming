3cf7a6a9ecc10f4de7730358732eedc5a80f2ba5
Collins Boakye <colly0087@gmail.com>
2024-02-19 12:23:37 -0800

    print a list in sorted order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A Module that inherits MyList from list """
+
+
+
+class MyList(list):
+    """
+    A Class that inherits from list
+
+    Attribute:
+        None Currently defined
+    Method:
+        None Currently defined
+
+    """
+    def print_sorted(self):
+        print (sorted(self))
