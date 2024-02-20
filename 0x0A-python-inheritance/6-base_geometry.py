9d3be8a4979ca427233791ad161c2e4cdcc95da7
Collins Boakye <colly0087@gmail.com>
2024-02-20 06:27:49 -0800

    raise exception when area not implemented

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" A Module that defines an empty class """
+
+
+class BaseGeometry(object):
+    """ An Empty class defined with no attributes and methods """
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
