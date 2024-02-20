e3ba6d36c48b3d0c8f750893418387f2217bfbe6
Collins Boakye <colly0087@gmail.com>
2024-02-20 08:08:23 -0800

    value validation with test case

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" A Module that defines an empty class """
+
+
+class BaseGeometry(object):
+    """ An Empty class defined with no attributes and methods """
+    pass
+
+    def area(self):
+        """ Raise an exception area is not implemented """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates that the 'value' is an integer greater than 0 """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
