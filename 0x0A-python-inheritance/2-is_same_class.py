a6a495e7ceb6179f098e198c9ec85969f922eaf9
Collins Boakye <colly0087@gmail.com>
2024-02-20 02:43:09 -0800

    check instance of an object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" A Module that check an instance of a class """
+
+
+def is_same_class(obj, a_class):
+    """ A Function that checks an object is an instance of a class """
+
+    return isinstance(obj, a_class)
