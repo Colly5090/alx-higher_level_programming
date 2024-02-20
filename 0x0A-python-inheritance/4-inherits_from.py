51d2e9d616544e9592d8384cec240b7825b03e11
Collins Boakye <colly0087@gmail.com>
2024-02-20 06:08:41 -0800

    inherited from the subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -8,11 +8,3 @@ def inherits_from(obj, a_class):
     (directly or indirectly)
     from the specified class a_class. """
     return issubclass(type(obj), a_class) and type(obj) is not a_class
-    
-a = True
-if inherits_from(a, int):
-    print("{} inherited from class {}".format(a, int.__name__))
-if inherits_from(a, bool):
-    print("{} inherited from class {}".format(a, bool.__name__))
-if inherits_from(a, object):
-    print("{} inherited from class {}".format(a, object.__name__))
