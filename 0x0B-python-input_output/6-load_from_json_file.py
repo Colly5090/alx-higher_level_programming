67bc8a3c3041e78f0bace272bcda8c54343aab29
Collins Boakye <colly0087@gmail.com>
2024-02-20 20:52:16 -0800

    load json data from file to object

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,4 +8,4 @@ import json
 def load_from_json_file(filename):
     """ A Function that loads an object from a JSOm file """
     with open(filename, 'r', encoding='utf-8') as file:
-        json.load(file)
+        return json.load(file)
