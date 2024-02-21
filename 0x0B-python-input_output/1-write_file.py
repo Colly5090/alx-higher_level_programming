2cf7829e6fdf6e37d7b5aee2e70fbb99d3db85d1
Collins Boakye <colly0087@gmail.com>
2024-02-20 19:19:19 -0800

    write to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" A Module that writes a string to a text file """
+
+
+def write_file(filename="", text=""):
+    """ A function to write a string to a UTF8 text file and return number of characters"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        numb_chars = file.write(text)
+    return numb_chars
