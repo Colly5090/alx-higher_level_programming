46d40d2cff65675b9d72c75f3f26c4db1ba9ee0c
Collins Boakye <colly0087@gmail.com>
2024-02-20 19:41:41 -0800

    json representation of an onject

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+""" A module that for JSON representation of an object """
+
+
+def to_json_string(my_obj):
+    """ A function that returns JSON representation of an onject """
+    return json.dumps(my_obj)
