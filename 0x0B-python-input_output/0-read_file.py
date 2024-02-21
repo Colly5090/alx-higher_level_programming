fe418ab8f7eec592f14660981784d8412b530742
Collins Boakye <colly0087@gmail.com>
2024-02-20 19:04:23 -0800

    Read from text file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """ A funtion that reads a UTF8 text files and print to stdout """
     with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+        print(file.read(), end='')
