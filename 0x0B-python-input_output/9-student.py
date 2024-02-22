63b50e9665b5a87c04c87174d0337572b7f46432
Collins Boakye <colly0087@gmail.com>
2024-02-21 20:53:32 -0800

    dictionary representation of student instance

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/bin/usr/python3
+""" A Module that defines a class of student """
+
+
+class Student:
+    """
+    A Class of Student that retrieves student attributes and
+    instance in dictionary representation
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ Return the dictionary representation of this Student instance """
+        return self.__dict__
