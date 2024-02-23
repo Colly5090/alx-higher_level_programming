aa4f62e962c27bbeb4092fd26c13f9a99c19edad
Collins Boakye <colly0087@gmail.com>
2024-02-23 01:53:42 -0800

    append a new_string

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" A Module to append a string in a text file """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """
+    A Function that inserts a line of text
+    to a file with a specific string
+    """
+    with open(filename, "r") as file:
+        lines = file.readlines()
+
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string + '\n')
