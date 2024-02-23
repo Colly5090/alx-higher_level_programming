dc15c2c221d37d8ed84bfddb93cfeb0a44e30884
Collins Boakye <colly0087@gmail.com>
2024-02-23 05:48:01 -0800

    inherit int built-in

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,7 +5,7 @@
 class MyInt(int):
     """ A class that define == and != in an inverted form """
     def __eq__(self, other):
-        return self.__ne__(self, other)
+        return not super().__eq__(other)
 
     def __ne__(self, other):
-        return self.__eq__(self, other)
+        return not super().__ne__(other)
