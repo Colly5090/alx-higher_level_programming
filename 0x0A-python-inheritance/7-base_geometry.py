0956628a59d5d25203516b880282e02727e593b2
Collins Boakye <colly0087@gmail.com>
2024-02-23 03:55:38 -0800

    value validation with test case

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -11,7 +11,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """ Validates that the 'value' is an integer greater than 0 """
-        if type(value) is not int or type(value) is bool:
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
