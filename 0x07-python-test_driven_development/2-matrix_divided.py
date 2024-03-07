f5069b1afabd75b14b17a2ea23c37a26097f7cd4
Collins Boakye <colly0087@gmail.com>
2024-03-07 02:58:42 -0800

    divide elements in a matrix with doctest

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,14 +4,16 @@
 
 def matrix_divided(matrix, div):
     """ A Function that divides each elements of a matrix """
-    if (type(matrix) != list or
-        not all(type(row) == list for row in matrix) or
-        not all(all(type(elem) in [int, float] for elem in row) for row in matrix)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    
+    if (not isinstance(matrix, list) or
+        not all(isinstance(row, list) for row in matrix) or
+        not all(all(isinstance(elem, (int, float)) for elem in row)
+                for row in matrix)):
+        raise TypeError(
+             "matrix must be a matrix (list of lists) of integers/floats")
+
     if any(len(row) != len(matrix[0]) for row in matrix):
-            raise TypeError("Each row of the matrix must have the same size")
-    if type(div) not in  [int, float]:
+        raise TypeError("Each row of the matrix must have the same size")
+    if type(div) not in [int, float]:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
