a5a314bf2cdf47e8d394c9e358324b65fe1d2d19
Collins Boakye <colly0087@gmail.com>
2024-03-07 04:52:16 -0800

    print square with # with doctest

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A Module to print square """
+
+
+def print_square(size):
+    """ A Function that prints square with # """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size <= 0:
+        raise ValueError("size must be >= 0")
+    elif size <= 0 and isinstance(size, float):
+        raise TypeError("size must be an integer")
+    else:
+        for _ in range(size):
+            print(size * "#")
