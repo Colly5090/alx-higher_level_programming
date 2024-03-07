74acfc2d87054375293538a0cab12feadea56f40
Collins Boakye <colly0087@gmail.com>
2024-03-07 06:40:11 -0800

    print string with 2 newlines with doctest

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" A Module that two new line after a text """
+
+
+def text_indentation(text):
+    """ A Function that prints newline after encountering . ? /"""
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    
+    new_str = text
+    for delimeter in ".?:":
+        new_str = new_str.replace(delimeter, delimeter + "\n\n\n")
+    
+    lines = new_str.split('\n')
+    for line in lines[:-1]:
+        print(line.strip())
