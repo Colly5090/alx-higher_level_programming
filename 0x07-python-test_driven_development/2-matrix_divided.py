6be33f1c27f1928916bd929d99bfcbbfe1733fe8
Collins Boakye <colly0087@gmail.com>
2024-03-07 02:40:05 -0800

    divide elements in a matrix with doctest

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+""" A Module that divides a matrix """
+
+
+def matrix_divided(matrix, div):
+    """ A Function that divides each elements of a matrix """
+    if (type(matrix) != list or
+        not all(type(row) == list for row in matrix) or
+        not all(all(type(elem) in [int, float] for elem in row) for row in matrix)):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    if any(len(row) != len(matrix[0]) for row in matrix):
+            raise TypeError("Each row of the matrix must have the same size")
+    if type(div) not in  [int, float]:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = [[round(elem / div, 2) for elem in row]for row in matrix]
+    return new_matrix
